843ad049b8644b6f63e7e03bd4b5968e353fbffc
Ebrahim <ebrahimali.cse.ru@gmail.com>
2021-06-09 03:06:30 +0600

    designing portfolio website

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.getElementById('ggg').addEventListener('click', () => {
+    console.log('clicked')
+})
+
+var typed3 = new Typed('.typing', {
+    strings: ['Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+
+//   SCROLL REVEAL
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+sr.reveal('.about__title', {})
+sr.reveal('.about__description', {delay: 200})
+sr.reveal('.about__photo', {delay: 100})
+
+sr.reveal('.skills__html', {interval: 30})
